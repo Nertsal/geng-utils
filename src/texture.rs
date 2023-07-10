f12cea6297a87fbf58593b407080f79882f62622
Nertsal <sasha.kudasov04@gmail.com>
2023-07-10 22:46:05 +0300

    Texture fit width/height

--- src/texture.rs
+++ src/texture.rs
@@ -21,6 +21,48 @@ pub fn attach_texture<'a>(texture: &'a mut ugli::Texture, ugli: &Ugli) -> ugli::
     ugli::Framebuffer::new_color(ugli, ugli::ColorAttachment::Texture(texture))
 }
 
+/// Fit a texture into the target [Aabb2] in world space.
+pub fn draw_texture_fit(
+    texture: &ugli::Texture,
+    target: Aabb2<f32>,
+    align: vec2<f32>,
+    camera: &impl geng::AbstractCamera2d,
+    geng: &Geng,
+    framebuffer: &mut ugli::Framebuffer,
+) {
+    let target = crate::layout::fit_aabb(texture.size().as_f32(), target, align);
+    geng.draw2d()
+        .textured_quad(framebuffer, camera, target, texture, Rgba::WHITE);
+}
+
+/// Fit a texture into the target [Aabb2] by width in world space.
+pub fn draw_texture_fit_width(
+    texture: &ugli::Texture,
+    target: Aabb2<f32>,
+    align: vec2<f32>,
+    camera: &impl geng::AbstractCamera2d,
+    geng: &Geng,
+    framebuffer: &mut ugli::Framebuffer,
+) {
+    let target = crate::layout::fit_aabb_width(texture.size().as_f32(), target, align);
+    geng.draw2d()
+        .textured_quad(framebuffer, camera, target, texture, Rgba::WHITE);
+}
+
+/// Fit a texture into the target [Aabb2] by height in world space.
+pub fn draw_texture_fit_height(
+    texture: &ugli::Texture,
+    target: Aabb2<f32>,
+    align: vec2<f32>,
+    camera: &impl geng::AbstractCamera2d,
+    geng: &Geng,
+    framebuffer: &mut ugli::Framebuffer,
+) {
+    let target = crate::layout::fit_aabb_height(texture.size().as_f32(), target, align);
+    geng.draw2d()
+        .textured_quad(framebuffer, camera, target, texture, Rgba::WHITE);
+}
+
 /// Fit a texture to the whole framebuffer.
 pub fn draw_texture_fit_screen(
     texture: &ugli::Texture,
@@ -38,16 +80,36 @@ pub fn draw_texture_fit_screen(
     );
 }
 
-/// Fit a texture into the target [Aabb2] in world space.
-pub fn draw_texture_fit(
+/// Fit a texture to the width of the framebuffer.
+pub fn draw_texture_fit_screen_width(
     texture: &ugli::Texture,
-    target: Aabb2<f32>,
     align: vec2<f32>,
-    camera: &impl geng::AbstractCamera2d,
     geng: &Geng,
     framebuffer: &mut ugli::Framebuffer,
 ) {
-    let target = crate::layout::fit_aabb(texture.size().as_f32(), target, align);
-    geng.draw2d()
-        .textured_quad(framebuffer, camera, target, texture, Rgba::WHITE);
+    draw_texture_fit_width(
+        texture,
+        Aabb2::ZERO.extend_positive(framebuffer.size().as_f32()),
+        align,
+        &geng::PixelPerfectCamera,
+        geng,
+        framebuffer,
+    );
+}
+
+/// Fit a texture to the height of the framebuffer.
+pub fn draw_texture_fit_screen_height(
+    texture: &ugli::Texture,
+    align: vec2<f32>,
+    geng: &Geng,
+    framebuffer: &mut ugli::Framebuffer,
+) {
+    draw_texture_fit_height(
+        texture,
+        Aabb2::ZERO.extend_positive(framebuffer.size().as_f32()),
+        align,
+        &geng::PixelPerfectCamera,
+        geng,
+        framebuffer,
+    );
 }

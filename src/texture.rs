3faa7279c22818c07cd17690c58bf14bca4429dc
Nertsal <sasha.kudasov04@gmail.com>
2023-07-10 22:28:10 +0300

    Texture fit

--- src/texture.rs
+++ src/texture.rs
@@ -1,5 +1,7 @@
 use geng::prelude::*;
 
+use crate::conversions::Vec2RealConversions;
+
 /// Construct a new black texture of the given size.
 pub fn new_texture(ugli: &Ugli, size: vec2<usize>) -> ugli::Texture {
     ugli::Texture::new_with(ugli, size, |_| Rgba::BLACK)
@@ -17,4 +19,35 @@ pub fn update_texture_size(texture: &mut ugli::Texture, size: vec2<usize>, ugli:
 /// Attach a texture to a color-only framebuffer.
 pub fn attach_texture<'a>(texture: &'a mut ugli::Texture, ugli: &Ugli) -> ugli::Framebuffer<'a> {
     ugli::Framebuffer::new_color(ugli, ugli::ColorAttachment::Texture(texture))
+}
+
+/// Fit a texture to the whole framebuffer.
+pub fn draw_texture_fit_screen(
+    texture: &ugli::Texture,
+    align: vec2<f32>,
+    geng: &Geng,
+    framebuffer: &mut ugli::Framebuffer,
+) {
+    draw_texture_fit(
+        texture,
+        Aabb2::ZERO.extend_positive(framebuffer.size().as_f32()),
+        align,
+        &geng::PixelPerfectCamera,
+        geng,
+        framebuffer,
+    );
+}
+
+/// Fit a texture into the target [Aabb2] in world space.
+pub fn draw_texture_fit(
+    texture: &ugli::Texture,
+    target: Aabb2<f32>,
+    align: vec2<f32>,
+    camera: &impl geng::AbstractCamera2d,
+    geng: &Geng,
+    framebuffer: &mut ugli::Framebuffer,
+) {
+    let target = crate::layout::fit_aabb(texture.size().as_f32(), target, align);
+    geng.draw2d()
+        .textured_quad(framebuffer, camera, target, texture, Rgba::WHITE);
 }

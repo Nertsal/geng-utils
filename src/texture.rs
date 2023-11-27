38afa58498c8e220c349992155743d0193125549
nertsal <nestralnertsal@gmail.com>
2023-11-27 14:22:54 +0300

    New texture draw api

--- src/texture.rs
+++ src/texture.rs
@@ -21,115 +21,97 @@ pub fn attach_texture<'a>(texture: &'a mut ugli::Texture, ugli: &Ugli) -> ugli::
     ugli::Framebuffer::new_color(ugli, ugli::ColorAttachment::Texture(texture))
 }
 
-/// Fit a texture into the target [Aabb2] in world space.
-pub fn draw_texture_fit(
-    texture: &ugli::Texture,
-    target: Aabb2<f32>,
-    align: vec2<f32>,
-    camera: &impl geng::AbstractCamera2d,
-    geng: &Geng,
-    framebuffer: &mut ugli::Framebuffer,
-) {
-    let target = crate::layout::fit_aabb(texture.size().as_f32(), target, align);
-    geng.draw2d()
-        .textured_quad(framebuffer, camera, target, texture, Rgba::WHITE);
+/// Helper for rendering textures.
+pub struct DrawTexture<'a> {
+    /// The texture to render.
+    pub texture: &'a ugli::Texture,
+    /// World position to render at.
+    pub target: Aabb2<f32>,
+    /// Color to render the texture with.
+    pub color: Rgba<f32>,
+    // TODO: DrawParameters
 }
 
-/// Fit a texture into the target [Aabb2] by width in world space.
-pub fn draw_texture_fit_width(
-    texture: &ugli::Texture,
-    target: Aabb2<f32>,
-    align: f32,
-    camera: &impl geng::AbstractCamera2d,
-    geng: &Geng,
-    framebuffer: &mut ugli::Framebuffer,
-) {
-    let target = crate::layout::fit_aabb_width(texture.size().as_f32(), target, align);
-    geng.draw2d()
-        .textured_quad(framebuffer, camera, target, texture, Rgba::WHITE);
-}
+impl<'a> DrawTexture<'a> {
+    pub fn new(texture: &'a ugli::Texture) -> Self {
+        Self {
+            texture,
+            target: Aabb2::ZERO.extend_positive(texture.size().as_f32()),
+            color: Rgba::WHITE,
+        }
+    }
 
-/// Fit a texture into the target [Aabb2] by height in world space.
-pub fn draw_texture_fit_height(
-    texture: &ugli::Texture,
-    target: Aabb2<f32>,
-    align: f32,
-    camera: &impl geng::AbstractCamera2d,
-    geng: &Geng,
-    framebuffer: &mut ugli::Framebuffer,
-) {
-    let target = crate::layout::fit_aabb_height(texture.size().as_f32(), target, align);
-    geng.draw2d()
-        .textured_quad(framebuffer, camera, target, texture, Rgba::WHITE);
-}
+    pub fn colored(self, color: Rgba<f32>) -> Self {
+        Self { color, ..self }
+    }
 
-/// Fit a texture to the whole framebuffer.
-pub fn draw_texture_fit_screen(
-    texture: &ugli::Texture,
-    align: vec2<f32>,
-    geng: &Geng,
-    framebuffer: &mut ugli::Framebuffer,
-) {
-    draw_texture_fit(
-        texture,
-        Aabb2::ZERO.extend_positive(framebuffer.size().as_f32()),
-        align,
-        &geng::PixelPerfectCamera,
-        geng,
-        framebuffer,
-    );
-}
+    /// Fit into the target [Aabb2] in world space and align.
+    pub fn fit(self, target: Aabb2<f32>, align: vec2<f32>) -> Self {
+        let target = crate::layout::fit_aabb(self.texture.size().as_f32(), target, align);
+        Self { target, ..self }
+    }
 
-/// Fit a texture to the width of the framebuffer.
-pub fn draw_texture_fit_screen_width(
-    texture: &ugli::Texture,
-    align: f32,
-    geng: &Geng,
-    framebuffer: &mut ugli::Framebuffer,
-) {
-    draw_texture_fit_width(
-        texture,
-        Aabb2::ZERO.extend_positive(framebuffer.size().as_f32()),
-        align,
-        &geng::PixelPerfectCamera,
-        geng,
-        framebuffer,
-    );
-}
+    /// Fit into the target [Aabb2] by width in world space and align vertically.
+    pub fn fit_width(self, target: Aabb2<f32>, align: f32) -> Self {
+        let target = crate::layout::fit_aabb_width(self.texture.size().as_f32(), target, align);
+        Self { target, ..self }
+    }
 
-/// Fit a texture to the height of the framebuffer.
-pub fn draw_texture_fit_screen_height(
-    texture: &ugli::Texture,
-    align: f32,
-    geng: &Geng,
-    framebuffer: &mut ugli::Framebuffer,
-) {
-    draw_texture_fit_height(
-        texture,
-        Aabb2::ZERO.extend_positive(framebuffer.size().as_f32()),
-        align,
-        &geng::PixelPerfectCamera,
-        geng,
-        framebuffer,
-    );
-}
+    /// Fit into the target [Aabb2] by height in world space and align horizontally.
+    pub fn fit_height(self, target: Aabb2<f32>, align: f32) -> Self {
+        let target = crate::layout::fit_aabb_height(self.texture.size().as_f32(), target, align);
+        Self { target, ..self }
+    }
 
-/// Draw a texture in a pixel-perfect way: such that pixels of the texture align with the pixels of the framebuffer.
-pub fn draw_pixel_perfect(
-    texture: &ugli::Texture,
-    pos: vec2<f32>,
-    align: vec2<f32>,
-    camera: &impl geng::AbstractCamera2d,
-    geng: &Geng,
-    framebuffer: &mut ugli::Framebuffer,
-) {
-    let pos = crate::pixel::pixel_perfect_aabb(
-        pos,
-        align,
-        texture.size(),
-        camera,
-        framebuffer.size().as_f32(),
-    );
-    geng.draw2d()
-        .textured_quad(framebuffer, camera, pos, texture, Rgba::WHITE);
+    /// Fit to the whole framebuffer.
+    pub fn fit_screen(self, align: vec2<f32>, framebuffer: &ugli::Framebuffer) -> Self {
+        self.fit(
+            Aabb2::ZERO.extend_positive(framebuffer.size().as_f32()),
+            align,
+        )
+    }
+
+    /// Fit to the width of the framebuffer.
+    pub fn fit_screen_width(self, align: f32, framebuffer: &mut ugli::Framebuffer) -> Self {
+        self.fit_width(
+            Aabb2::ZERO.extend_positive(framebuffer.size().as_f32()),
+            align,
+        )
+    }
+
+    /// Fit to the height of the framebuffer.
+    pub fn fit_screen_height(self, align: f32, framebuffer: &mut ugli::Framebuffer) -> Self {
+        self.fit_height(
+            Aabb2::ZERO.extend_positive(framebuffer.size().as_f32()),
+            align,
+        )
+    }
+
+    /// Align to pixel-perfect: such that pixels of the texture align with the pixels of the framebuffer.
+    pub fn pixel_perfect(
+        self,
+        pos: vec2<f32>,
+        align: vec2<f32>,
+        camera: &impl geng::AbstractCamera2d,
+        framebuffer: &mut ugli::Framebuffer,
+    ) -> Self {
+        let target = crate::pixel::pixel_perfect_aabb(
+            pos,
+            align,
+            self.texture.size(),
+            camera,
+            framebuffer.size().as_f32(),
+        );
+        Self { target, ..self }
+    }
+
+    pub fn draw(
+        self,
+        camera: &impl geng::AbstractCamera2d,
+        geng: &Geng,
+        framebuffer: &mut ugli::Framebuffer,
+    ) {
+        geng.draw2d()
+            .textured_quad(framebuffer, camera, self.target, self.texture, self.color);
+    }
 }

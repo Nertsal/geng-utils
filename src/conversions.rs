13dd0bbd330e93a035f87e1cbbec3ace2070a43a
nertsal <nestralnertsal@gmail.com>
2026-07-15 21:23:11 +0300

    add more conversions

--- src/conversions.rs
+++ src/conversions.rs
@@ -1,4 +1,4 @@
-use geng::prelude::{mat3, r32, vec2, Aabb2, Angle, Float, R32, R64};
+use geng::prelude::{Aabb2, Angle, Float, R32, R64, mat3, mat4, r32, vec2, vec3, vec4};
 
 /// A trait for converting into an [R32].
 pub trait RealConversions {
@@ -23,6 +23,18 @@ pub trait Vec2RealConversions {
     fn as_r32(&self) -> vec2<R32>;
 }
 
+/// A trait for converting [vec3]'s inner types between [f32] and [R32].
+pub trait Vec3RealConversions {
+    fn as_f32(&self) -> vec3<f32>;
+    fn as_r32(&self) -> vec3<R32>;
+}
+
+/// A trait for converting [vec4]'s inner types between [f32] and [R32].
+pub trait Vec4RealConversions {
+    fn as_f32(&self) -> vec4<f32>;
+    fn as_r32(&self) -> vec4<R32>;
+}
+
 /// A trait for converting [Aabb2]'s inner types between [f32] and [R32].
 pub trait Aabb2RealConversions {
     fn as_f32(&self) -> Aabb2<f32>;
@@ -33,6 +45,12 @@ pub trait Aabb2RealConversions {
 pub trait Mat3RealConversions {
     fn as_f32(&self) -> mat3<f32>;
     fn as_r32(&self) -> mat3<R32>;
+}
+
+/// A trait for converting [mat4]'s inner types between [f32] and [R32].
+pub trait Mat4RealConversions {
+    fn as_f32(&self) -> mat4<f32>;
+    fn as_r32(&self) -> mat4<R32>;
 }
 
 macro_rules! impl_lossy {
@@ -91,6 +109,10 @@ impl_all_float!(AngleRealConversions, Angle);
 
 impl_all_lossy!(Vec2RealConversions, vec2);
 impl_all_float!(Vec2RealConversions, vec2);
+impl_all_lossy!(Vec3RealConversions, vec3);
+impl_all_float!(Vec3RealConversions, vec3);
+impl_all_lossy!(Vec4RealConversions, vec4);
+impl_all_float!(Vec4RealConversions, vec4);
 
 impl_all_lossy!(Aabb2RealConversions, Aabb2);
 impl_all_float!(Aabb2RealConversions, Aabb2);
@@ -100,6 +122,15 @@ impl<T: Float> Mat3RealConversions for mat3<T> {
         self.map(|x| x.as_f32())
     }
     fn as_r32(&self) -> mat3<R32> {
+        self.map(|x| r32(x.as_f32()))
+    }
+}
+
+impl<T: Float> Mat4RealConversions for mat4<T> {
+    fn as_f32(&self) -> mat4<f32> {
+        self.map(|x| x.as_f32())
+    }
+    fn as_r32(&self) -> mat4<R32> {
         self.map(|x| r32(x.as_f32()))
     }
 }
